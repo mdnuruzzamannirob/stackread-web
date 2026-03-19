36247757cb45a7548c3a49002b720b0a4c6095cd
Md. Nuruzzaman <dev.mdnuruzzaman@gmail.com>
2026-03-19 10:55:13 +0600

    feat: integrate Sonner for toast notifications and update Providers component

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
+import { Toaster } from 'sonner'
+
 import { AuthHydrator } from '@/components/providers/AuthHydrator'
 import { ReduxProvider } from '@/components/providers/ReduxProvider'
 import { ThemeProvider } from '@/components/providers/ThemeProvider'
@@ -10,11 +13,24 @@ type ProvidersProps = {
 
 export function Providers({ children }: ProvidersProps) {
   return (
-    <ThemeProvider>
-      <ReduxProvider>
-        <AuthHydrator />
-        {children}
-      </ReduxProvider>
-    </ThemeProvider>
+    <NuqsAdapter>
+      <ThemeProvider>
+        <ReduxProvider>
+          <AuthHydrator />
+
+          {children}
+
+          <Toaster
+            position="top-center"
+            richColors
+            expand
+            toastOptions={{
+              className:
+                'rounded-xl border border-border bg-background text-foreground shadow-lg',
+            }}
+          />
+        </ReduxProvider>
+      </ThemeProvider>
+    </NuqsAdapter>
   )
 }
